5e53740eeee6614a7d653930a97b4a5eafd7120b
ladisho <20084356@mail.wit.ie>
2021-11-11 13:34:39 +0000

    logging event handling completed

--- app/src/main/java/org/wit/car/CarActivity.kt
+++ app/src/main/java/org/wit/car/CarActivity.kt
@@ -2,10 +2,16 @@ package org.wit.car
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.i
 
 class CarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_car)
+
+        Timber.plant(Timber.DebugTree())
+
+        i("Car Activity started...")
     }
 }
