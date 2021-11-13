8302ecd4d4fecf93e9b291210b1340638beb4eb1
ladisho <20084356@mail.wit.ie>
2021-11-13 19:18:03 +0000

    Validation implemented

--- app/src/main/java/org/wit/car/activities/CarActivity.kt
+++ app/src/main/java/org/wit/car/activities/CarActivity.kt
@@ -30,13 +30,18 @@ class CarActivity : AppCompatActivity() {
         binding.btnAdd.setOnClickListener() {
             car.model = binding.carModel.text.toString()
             car.brand = binding.carBrand.text.toString()
-            if (binding.carYear.text.isNotEmpty()){
+            if (binding.carYear.text.isNotEmpty() && binding.carYear.text.length == 4){
                 car.year = Integer.parseInt(binding.carYear.text.toString())
             }else{
                 car.year = 9999
             }
 
-            car.plateNumber = binding.carPlateNumber.text.toString()
+            if(binding.carPlateNumber.text.isNotEmpty() && binding.carPlateNumber.text.length == 11){
+                car.plateNumber = binding.carPlateNumber.text.toString()
+            }else{
+                car.plateNumber = "999-W-99999"
+            }
+
             var msg:String =""
             if (car.model.isNotEmpty()) {
                 i("add Button Pressed: ${car.model}")
@@ -70,9 +75,7 @@ class CarActivity : AppCompatActivity() {
 //                    .show()
             }
             if (car.plateNumber.isNotEmpty()) {
-                i("add Button Pressed: ${car.model} ${car.brand} ${car.year} ${car.plateNumber}")
-                cars.add(car.copy())
-                i("$car")
+                i("add Button Pressed: ${car.plateNumber}")
             }
             else {
                 msg += "Please enter a plateNumber"
@@ -80,6 +83,12 @@ class CarActivity : AppCompatActivity() {
 //                    .make(it,"Please Enter a brand", Snackbar.LENGTH_LONG)
 //                    .show()
             }
+            if (car.model.isNotEmpty() && car.brand.isNotEmpty() && car.year > 0 && car.plateNumber.isNotEmpty()){
+                i("add Button Pressed: ${car.model} ${car.brand} ${car.year} ${car.plateNumber}")
+                cars.add(car.copy())
+                i("$car")
+            }
+
             if(msg.isNotEmpty()){
                 Snackbar
                     .make(it,msg, Snackbar.LENGTH_LONG)
