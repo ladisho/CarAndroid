b691850dd58dd5f2d2b287898ad65c47c430e308
ladisho <20084356@mail.wit.ie>
2021-11-18 13:31:32 +0000

    Parcelable implemented

--- app/src/main/java/org/wit/car/models/CarModel.kt
+++ app/src/main/java/org/wit/car/models/CarModel.kt
@@ -1,7 +1,11 @@
 package org.wit.car.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class CarModel(var model: String = "",
                     var brand: String = "",
                     var year: Int = 0,
                     var plateNumber: String = ""
-)
+) : Parcelable
